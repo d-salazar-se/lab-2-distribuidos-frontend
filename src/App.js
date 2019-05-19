08c2016b04ade7d458dac57daea271b099174c3b
Cristobal <cristobal.donoso.s@usach.cl>
2019-05-18 20:31:21 -0400

    create app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ActionBar from './components/actionBar/index.js';
+
+function App() {
+  return (
+    <div className="App">
+          <ActionBar> </ActionBar>
+    </div>
+  );
+}
+
+export default App;
